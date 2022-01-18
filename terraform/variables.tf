ca98338c4379af2c72802e9b2f4a09e77dca6c7e
xiongfeng-code <97938315+xiongfeng-code@users.noreply.github.com>
2022-01-18 14:49:10 +0800

    Update variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,11 +1,11 @@
 variable "resource_group" {
   description = "The resource group"
-  default = "CHANGE_ME_RESOURCE_GROUP"
+  default = "event_group"
 }
 
 variable "application_name" {
   description = "The Spring Boot application name"
-  default     = "CHANGE_ME_APP_NAME"
+  default     = "app-xf-p"
 }
 
 variable "location" {
