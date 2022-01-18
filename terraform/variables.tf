537b840958cffe76168a035dc5ebb13a7fc8314b
xiongfeng-code <97938315+xiongfeng-code@users.noreply.github.com>
2022-01-18 14:52:45 +0800

    Update variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -5,7 +5,7 @@ variable "resource_group" {
 
 variable "application_name" {
   description = "The Spring Boot application name"
-  default     = "xf_name"
+  default     = "xfname"
 }
 
 variable "location" {
